7d7188a2562e3a602d069237185b346c7c5611c2
Sofia Genchi <sofigen16@gmail.com>
2023-11-27 22:04:20 -0300

    instalando tailwind

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,37 +1,40 @@
-import { useState } from "react"
+import { useState } from "react";
 import TaskForm from "./TaskForm";
 import TaskItem from "./TaskItem";
 
 export default function TaskList() {
   const [list, setList] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
-  function handleChange(e){
-    setInputValue(e.target.value)
+  function handleChange(e) {
+    setInputValue(e.target.value);
   }
 
-  function handleSubmit(e){
-    e.preventDefault()
-    setList([...list, inputValue])
-    setInputValue('')
+  function handleSubmit(e) {
+    e.preventDefault();
+    setList([...list, inputValue]);
+    setInputValue("");
   }
 
-  function handleDelete(index){
+  function handleDelete(index) {
     const newItems = [...list];
-    newItems.splice(index, 1)
+    newItems.splice(index, 1);
     setList(newItems);
   }
 
-
   return (
     <div>
-    <h1>Todo List</h1>
-    <TaskForm handleSubmit={handleSubmit} handleChange={handleChange} inputValue={inputValue} />
-    <ul>
+      <h1>Todo List</h1>
+      <TaskForm
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        inputValue={inputValue}
+      />
+      <ul>
         {list.map((item, index) => (
-            <TaskItem item={item} handleDelete={handleDelete} index={index} />
+          <TaskItem item={item} handleDelete={handleDelete} index={index} />
         ))}
-    </ul>
-</div>
-  )
+      </ul>
+    </div>
+  );
 }
